2bce5dac425af92e86b136469be2039bbb4172e9
Niloy-D-DG <niloy.dasgupta@accenture.com>
2026-03-06 10:20:01 +0000

    Fourth Commit

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,17 @@ SECRET_KEY = 'django-insecure-5=zn9ctz(4wo_9_ri+f0s$8h#n%&-&pk*cc(yy1(57i4@h+*py
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+import os
+codespace_name = os.environ.get('CODESPACE_NAME')
+if codespace_name:
+    ALLOWED_HOSTS = [
+        f"{codespace_name}-8000.app.github.dev",
+        'localhost',
+        '127.0.0.1',
+        '[::1]'
+    ]
+else:
+    ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]']
 
 
 # Application definition
